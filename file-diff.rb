b241e687699122cbfe4f7cc2b6583f847231a629
Derek Frankhouser <derek.frankhouser@gmail.com>
2018-12-01 20:52:58 -0500

    Improve memory efficiency

--- file-diff.rb
+++ file-diff.rb
@@ -32,6 +32,16 @@ DELMS = { "TAB" => "\t" , "COMMA" => "," , "PIPE" => "|" }
 
 require_relative INCLUDES_DIR + "input.rb"
 
+##################
+## Methods
+##################
+
+def buildKey( data , keys , blank_value = "<BLANK_FIELD>" )
+  tmp = []
+  keys.each{ |k| tmp << ( data[k] || "<BLANK_FIELD>" ) }
+  return tmp.join(KEY_DELIM)
+end
+
 #################
 ## Setup output
 #################
@@ -48,28 +58,22 @@ else
   tmp_del = $inputs[:delimiter]
 end
 
-data = {}
-[ :first_file , :second_file].each do |fn|
-  tmp_file_name = fn.to_s.gsub("_"," ")
-  tmp_file = $inputs[fn]
-  tmp_lines = (File.open(tmp_file , "r").readlines.size) - 1
-  $log.info("Loading #{tmp_file_name} to memory (Number of lines: #{tmp_lines})...")
-  data[fn] = {}
-  progressbar = ProgressBar.create(:title => "Loading #{tmp_file_name}...", :starting_at => 0, :total => tmp_lines , :format => PROG_FORMAT , :output => $stderr )
-  CSV.foreach( tmp_file , :col_sep => tmp_del , :headers => true) do |row|
-    # TODO: Check headers
-    row = row.to_hash
-    tmp_key_arr = []
-    $inputs[:key_cols].each do |k|
-      tmp_key_arr << ( row[k] || "<BLANK_FIELD>" )
-    end
-    tmp_key = tmp_key_arr.join(KEY_DELIM)
-    raise "Key #{tmp_key} is not unique in #{tmp_file_name}." if data[fn].has_key?(tmp_key)
-    data[fn][tmp_key] = row
-    progressbar.increment
-  end
-  $log.info("...#{tmp_file_name} loaded.")
+first_file_keys = {}
+second_file_data = {}
+first_file_size = (File.open($inputs[:first_file] , "r").readlines.size) - 1
+second_file_size = (File.open($inputs[:second_file] , "r").readlines.size) - 1
+
+$log.info("Loading second file to memory (Number of lines: #{second_file_size})...")
+progressbar = ProgressBar.create(:title => "Loading second file...", :starting_at => 0, :total => second_file_size , :format => PROG_FORMAT , :output => $stderr )
+CSV.foreach( $inputs[:second_file] , :col_sep => tmp_del , :headers => true) do |row|
+  # TODO: Check headers
+  row = row.to_hash
+  tmp_key = buildKey( row , $inputs[:key_cols] )
+  raise "Key #{tmp_key} is not unique in second file." if second_file_data.has_key?(tmp_key)
+  second_file_data[tmp_key] = row
+  progressbar.increment
 end
+$log.info("...second file loaded.")
 
 ########################################
 ## Open files
@@ -80,49 +84,61 @@ singletons ={}
   singletons[file] = CSV.open(OUTPUT_DIR + "/" + file.to_s + "_only.txt",'w' ,
       :col_sep => tmp_del ,
       :write_headers => true,
-      :headers => data[file].first[1].keys
+      :headers => second_file_data.first[1].keys
       )
 end
 
 diffs = CSV.open(OUTPUT_DIR + "/" + "diffs.txt",'w' ,
   :col_sep => tmp_del ,
   :write_headers => true,
-  :headers => $inputs[:key_cols].concat(["COLUMN","FIRST_FILE","SECOND_FILE"])
+  :headers => $inputs[:key_cols].clone.concat(["COLUMN","FIRST_FILE","SECOND_FILE"])
   )
 
 ########################################
 ## Find differances
 ########################################
 
-[
-  [:first_file , :second_file],
-  [:second_file , :first_file]
-].each do |file_combo|
-  $log.info("Comparing #{file_combo[0]} to #{file_combo[1]}...")
-  progressbar = ProgressBar.create(:title => "Comparing #{file_combo[0].to_s.gsub("_file","")} to #{file_combo[1].to_s.gsub("_file","")}...", :starting_at => 0, :total => data[file_combo[0]].size, :format => PROG_FORMAT, :output => $stderr )
-  data[file_combo[0]].each do |k,d|
-    # Singleton: check if key exists in other file
-    if data[file_combo[1]].has_key?(k)
-      # Only performing diffing logic on first pass
-      if file_combo[0] == :first_file
-        tmp_diffs = diff = HashDiff.diff(data[file_combo[0]][k], data[file_combo[1]][k])
-        tmp_diffs.each do |d|
-          tmp_ignore = false
-          $inputs[:ignored_columns].each do |c|
-              tmp_ignore = true if /#{c}/ =~ d[1]
-          end
-          if not tmp_ignore
-            diffs << k.split(KEY_DELIM).concat([ d[1] , d[2] , d[3] ])
-          end
-        end
+$log.info("Comparing first file to second file...")
+progressbar = ProgressBar.create(:title => "Comparing first to second...", :starting_at => 0, :total => first_file_size, :format => PROG_FORMAT, :output => $stderr )
+CSV.foreach( $inputs[:first_file] , :col_sep => tmp_del , :headers => true) do |first_file_row|
+  # TODO: Check headers
+  first_file_row = first_file_row.to_hash
+  k = buildKey( first_file_row , $inputs[:key_cols] )
+  # Singleton: check if key exists in other file
+  if first_file_keys.has_key?(k)
+    raise "Key #{k} is not unique in second file."
+  else
+    first_file_keys[k] = true
+  end
+
+  if second_file_data.has_key?(k)
+    tmp_diffs = diff = HashDiff.diff( first_file_row , second_file_data[k])
+    tmp_diffs.each do |d|
+      tmp_ignore = false
+      $inputs[:ignored_columns].each do |c|
+          tmp_ignore = true if /#{c}/ =~ d[1]
       end
-    else
-      singletons[file_combo[0]] << d.values
+      if not tmp_ignore
+        diffs << k.split(KEY_DELIM).concat([ d[1] , d[2] , d[3] ])
+      end
     end
+  else
+    singletons[:first_file] << first_file_row.values
+  end
+  progressbar.increment
+end
+$log.info("...files compared.")
+
+
+$log.info("Comparing second file to first file...")
+progressbar = ProgressBar.create(:title => "Comparing second to first...", :starting_at => 0, :total => second_file_size, :format => PROG_FORMAT, :output => $stderr )
+second_file_data.each do |k,d|
+  if !first_file_keys.has_key?(k)
+    singletons[:second_file] << d.values
     progressbar.increment
   end
-  $log.info("...files compared.")
 end
+$log.info("...files compared.")
 
 # Close the files
 singletons.each{ |k,v| v.close }
